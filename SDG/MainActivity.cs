b1d59ed1f6a1490e8ef199546d2df419afd33ba7
장지훈 <s5v7@outlook.kr>
2018-05-10 21:32:11 +0900

    Basic Setting

--- SDG/MainActivity.cs
+++ SDG/MainActivity.cs
@@ -2,18 +2,15 @@
 using Android.Widget;
 using Android.OS;
 
-namespace SDG
-{
-    [Activity(Label = "SDG", MainLauncher = true)]
-    public class MainActivity : Activity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+namespace SDG {
+	[Activity(Label = "SDG", MainLauncher = true)]
+	public class MainActivity : Activity {
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-        }
-    }
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+	}
 }
 
