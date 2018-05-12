c1242a79e7e7d0957fa4229096e1db130e8dcd40
장지훈 <s5v7@outlook.kr>
2018-05-12 19:30:10 +0900

    Add Register Method on UserManager

--- SDG-Site/managers/UserManager.cs
+++ SDG-Site/managers/UserManager.cs
@@ -5,5 +5,31 @@ namespace SDG_Site.Managers {
 	public static class UserManager {
 		// Table Name
 		private const string USERTABLE = "users";
+
+
+		/// <summary>
+		/// Register by <c>User</c> class
+		/// </summary>
+		/// <param name="user">User class in Models</param>  
+		/// <see cref="User"/>
+		public static int Register(User user) {
+			// Connect to DB
+			using (var conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["sdgDB"].ConnectionString)) {
+				conn.Open();
+
+				// Command Text - Insert new user
+				string commandText = "INSERT INTO " + USERTABLE + "(User_Id, Password, Name) VALUES (" + user.UserID + ", " + user.Password + ", " + user.Name + ")";
+				var cmd = new MySqlCommand(commandText, conn);
+
+				// The number of rows affected
+				int result = cmd.ExecuteNonQuery();
+
+				// Connection Close
+				conn.Close();
+
+				return result;
+			}
+		}
+
 	}
 }
