e4f514de0256427447a02ae8be1635345e39b3c9
장지훈 <s5v7@outlook.kr>
2018-05-12 21:59:19 +0900

    Add Default Page and Login Check

---
+++ SDG-Site/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SDG_Site.Default" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title></title>
+</head>
+<body>
+	<form runat="server">
+	</form>
+</body>
+</html>
---
+++ SDG-Site/Default.aspx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SDG_Site {
+	public partial class Default : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			// Redirect to the Login page if the session does not exist
+			if (String.IsNullOrEmpty((string)Session["UserID"]))
+				Response.Redirect("/Login.aspx");
+		}
+	}
+}
