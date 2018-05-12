8e31e08a722f08acef98fcd631a3903645fbcea7
장지훈 <s5v7@outlook.kr>
2018-05-12 21:20:58 +0900

    Add Logout Page

---
+++ SDG-Site/Logout.aspx.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SDG_Site {
+	public partial class Logout : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			Session["UserID"] = "";
+		}
+	}
+}
