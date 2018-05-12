749dc564e5b1ac66cfd10d2fd1a3d2b07ddb8d90
장지훈 <s5v7@outlook.kr>
2018-05-12 19:29:38 +0900

    Add User Manager

---
+++ SDG-Site/managers/UserManager.cs
@@ -0,0 +1,9 @@
+﻿using MySql.Data.MySqlClient;
+using SDG_Site.Models;
+
+namespace SDG_Site.Managers {
+	public static class UserManager {
+		// Table Name
+		private const string USERTABLE = "users";
+	}
+}
