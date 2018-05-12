c0ce50e64022f6a9d43eedf1bc851c8b803ad4ed
장지훈 <s5v7@outlook.kr>
2018-05-12 19:28:22 +0900

    Add user's Model

---
+++ SDG-Site/models/User.cs
@@ -0,0 +1,29 @@
+﻿namespace SDG_Site.Models {
+	/// <summary>
+	/// users table Model
+	/// </summary>
+	public class User {
+		// User's Serial Number
+		public int Id { get; set; }
+
+		// User's ID
+		public string UserID { get; set; }
+
+		// User's Password
+		public string Password {
+			get {
+				// password Returns a hash value on request
+				return Password.GetHashCode().ToString();
+			}
+			set {
+				Password = value;
+			}
+		}
+
+		// User's Name
+		public string Name { get; set; }
+		
+		// User's Uni
+		public int Uni { get; set; }
+	}
+}
