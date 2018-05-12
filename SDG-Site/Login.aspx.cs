eed50284d44f2ec4b398f7d22e0373102976c6eb
장지훈 <s5v7@outlook.kr>
2018-05-12 19:31:21 +0900

    Add login Page
    basic page not design

---
+++ SDG-Site/Login.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SDG_Site {
+	public partial class Login : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+
+		}
+
+		protected void SignInButton_Click(object sender, EventArgs e) {
+
+		}
+	}
+}
