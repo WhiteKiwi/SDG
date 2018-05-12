e7d8dad4c48b4775ce446de3af07fa564b23ff3d
장지훈 <s5v7@outlook.kr>
2018-05-12 19:31:45 +0900

    Add Login.axml

--- SDG/Resources/Resource.Designer.cs
+++ SDG/Resources/Resource.Designer.cs
@@ -45,7 +45,10 @@ namespace SDG
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int Login = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Main = 2130837505;
 			
 			static Layout()
 			{
