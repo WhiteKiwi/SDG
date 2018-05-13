d460cafead318533ef5696da0c1dcdc541f733ba
장지훈 <s5v7@outlook.kr>
2018-05-13 20:11:21 +0900

    Redirect to login page after logout

--- SDG-Site/Logout.aspx.cs
+++ SDG-Site/Logout.aspx.cs
@@ -4,6 +4,8 @@ namespace SDG_Site {
 	public partial class Logout : System.Web.UI.Page {
 		protected void Page_Load(object sender, EventArgs e) {
 			Session["UserID"] = "";
+
+			Response.Redirect("/Login.aspx");
 		}
 	}
 }
