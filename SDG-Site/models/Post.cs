ef0212f4132db578ec4ca097f510019f7b23b097
장지훈 <s5v7@outlook.kr>
2018-05-19 11:12:52 +0900

    Add posts model

---
+++ SDG-Site/models/Post.cs
@@ -0,0 +1,21 @@
+﻿namespace SDG_Site.Models {
+	/// <summary>
+	/// posts table Model
+	/// </summary>
+	public class Post {
+		// Post's ID
+		public int Id { get; set; }
+
+		// Title
+		public string Title { get; set; }
+
+		// Content
+		public string Content { get; set; }
+
+		// Classification
+		public string Classification { get; set; }
+
+		// Stage
+		public int Stage { get; set; }
+	}
+}
