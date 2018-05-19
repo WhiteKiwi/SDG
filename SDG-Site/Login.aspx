9e4bc908dcaa45469e2eb529b1f8ebd270e61837
장지훈 <s5v7@outlook.kr>
2018-05-19 11:12:37 +0900

    Change class name

--- SDG-Site/Login.aspx
+++ SDG-Site/Login.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SDG_Site.Login1" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SDG_Site.Login" %>
 
 <!DOCTYPE html>
 
--- SDG-Site/Login.aspx.cs
+++ SDG-Site/Login.aspx.cs
@@ -2,7 +2,7 @@
 using System;
 
 namespace SDG_Site {
-    public partial class Login1 : System.Web.UI.Page {
+    public partial class Login : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
 
         }
@@ -11,7 +11,7 @@ namespace SDG_Site {
 			// If the login succeeds, save the value to the session and return to the main page
 			if (UserManager.LoginCheck(UserID.Text, Password.Text)) {
 				Session["UserID"] = UserID.Text;
-
+				
 				Response.Redirect("/");
 			} else {
 				Response.Write("<script>alert('Please check ID and PW');</script>");
