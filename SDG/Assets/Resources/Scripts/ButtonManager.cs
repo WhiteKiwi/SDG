2fe8e1a6304ded9f559c3f705ab5625c3dd42d9a
장지훈 <s5v7@outlook.kr>
2018-06-09 10:56:24 +0900

    AddUnity

---
+++ SDG/Assets/Resources/Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+	// Scene Main - Start Button
+	public void Press_StartButton() {
+		int stage = PlayerPrefs.GetInt("Stage", 1);
+
+		if (stage == 1) {
+			PlayerPrefs.SetInt("Stage", 1);
+			PlayerPrefs.Save();
+			SceneManager.LoadScene("Stage" + stage.ToString());
+		}
+	}
+}
