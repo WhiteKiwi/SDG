cf2ea4cffb5f5fe5bbcde28944b4755a062059cd
plegaspi <plegaspi07@gmail.com>
2018-06-26 00:29:47 -1000

    Created a responsive navbar.

---
+++ SDG-Site/Website Front End/src/scripts/app.js
@@ -0,0 +1,9 @@
+function showNav() {
+  console.log("this is working");
+  var x = document.getElementById("responsive-nav");
+  if (x.className === "responsive-nav") {
+    x.className += " unfold";
+  } else {
+    x.className = "responsive-nav";
+  }
+}
